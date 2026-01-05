fe57af48e67619e1f211d8721fb3d60eeba5da8b
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-01-05 16:23:07 +0100

    refactor(app): Moved styles inside component

--- my-first-app/components/CreateZone.jsx
+++ my-first-app/components/CreateZone.jsx
@@ -1,6 +1,32 @@
 import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
 
 export const CreateZone = ({ newObjective, handleChange, handleAddGoal }) => {
+    
+    const styles = StyleSheet.create({
+    
+        input: {
+            height: "100%",
+            borderColor: "gray",
+            borderRadius: 2,
+            borderWidth: 1,
+            paddingHorizontal: 10,
+            paddingVertical: 2,
+        },
+    
+        addButtonText: {
+            color: "white",
+            fontWeight: "bold",
+            textAlign: "center",
+        },
+    
+        addButton: {
+            backgroundColor: "rgba(194, 71, 46, 1)",
+            paddingVertical: 6,
+            paddingHorizontal: 12,
+            borderRadius: 4,
+        },
+    });
+    
     return (
         <View
             style={{
@@ -26,29 +52,3 @@ export const CreateZone = ({ newObjective, handleChange, handleAddGoal }) => {
         </View>
     );
 };
-
-
-const styles = StyleSheet.create({
-
-    input: {
-        height: "100%",
-        borderColor: "gray",
-        borderRadius: 2,
-        borderWidth: 1,
-        paddingHorizontal: 10,
-        paddingVertical: 2,
-    },
-
-    addButtonText: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-
-    addButton: {
-        backgroundColor: "rgba(194, 71, 46, 1)",
-        paddingVertical: 6,
-        paddingHorizontal: 12,
-        borderRadius: 4,
-    },
-});
